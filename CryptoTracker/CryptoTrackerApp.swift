65f247d12045441a5b0972f4ef7ee4d2d84e724a
Chiraphat Techasiri <ctechasiri@gmail.com>
2025-11-03 13:41:26 -0500

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Chiraphat Techasiri on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
