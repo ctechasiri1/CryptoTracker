f33c134c97ad56487847b4a86df1392ec1968c36
Chiraphat Techasiri <ctechasiri@gmail.com>
2025-12-08 18:22:33 -0800

    Finished the rest of the settings section

--- CryptoTracker/Core/Settings/Views/SettingsView.swift
+++ CryptoTracker/Core/Settings/Views/SettingsView.swift
@@ -9,18 +9,33 @@ import SwiftUI
 
 struct SettingsView: View {
     @EnvironmentObject private var viewModel: HomeViewModel
+    private let defaultURL: String = "https://github.com/chiraphattechasiri/CryptoTracker"
     var body: some View {
         NavigationStack {
             List {
-                Section {
-                    Text("hello")
+                Section("COINGECKO") {
+                    VStack {
+                        Image("coingecko")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 100)
+                        
+                        Text("The crytocurrency data that is used in this app comes from a free API from CoinGecko! Prices may be slightly delayed!")
+                    }
+                    if let url = URL(string: "https://www.coingecko.com") {
+                        Link("Visit CoinGecko", destination: url)
+                            .foregroundStyle(Color.blue)
+                    }
                 }
                 
-                Section {
-                    Text("hello")
+                Section("APPLICATION") {
+                    if let url = URL(string: defaultURL) {
+                        Link("Terms of Service", destination: url)
+                        Link("Privacy Policy", destination: url)
+                        Link("Company Website", destination: url)
+                        Link("Learn More", destination: url)
+                    }
                 }
-                
-                
             }
             .listStyle(.grouped)
             .toolbar {
